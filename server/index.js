0beb09621f9eaa89db199a23d0c378895e819f59
Wilheim <willabraham1@gmail.com>
2022-11-21 19:58:20 +1100

    updated server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
